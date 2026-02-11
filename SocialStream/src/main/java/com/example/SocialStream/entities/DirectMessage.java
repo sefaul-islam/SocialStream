7471b53e549e8413adf9f593a3ca2a20496e0a71
sefaul-islam <sefaulislam.off@gmail.com>
2026-02-11 14:50:06 +0600

    added index in the directmessage entity

--- SocialStream/src/main/java/com/example/SocialStream/entities/DirectMessage.java
+++ SocialStream/src/main/java/com/example/SocialStream/entities/DirectMessage.java
@@ -12,17 +12,23 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "OnetoOneMessages")
+@Table(name = "OnetoOneMessages",indexes ={
+        @Index(name = "idx_message_sender_recipient_time",columnList = "sender_id,recipient_id,send_time")
+})
 public class DirectMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false, updatable = false)
     private User sender;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_id", nullable = false, updatable = false)
     private User recipient;
+
     private String content;
+    @Column(name = "send_time", nullable = false, updatable = false)
     private LocalDateTime sendTime;
     @Enumerated(EnumType.STRING)
     private Reaction reaction;
